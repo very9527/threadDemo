6182926a182dccfd7527ca193ed4e00c22fc2d12
very9527 <zhangzhiyun87@gmail.com>
2015-11-05 21:37:29 +0800

    add ThreadLocal

---
+++ src/com/threadLocal/ThreadLocalDemo.java
@@ -0,0 +1,41 @@
+package com.threadLocal;
+
+import java.util.Random;
+
+/**
+ * ThreadLocal 的使用
+ * @author very9527
+ *
+ */
+public class ThreadLocalDemo {
+	private static ThreadLocal threadLocal = new ThreadLocal();
+	public static void main(String[] args) {
+		for (int i = 0; i<2; i++) {
+			new Thread(new Runnable(){
+				@Override
+				public void run() {
+					int data = new Random().nextInt();
+					System.out.println(Thread.currentThread().getName() + " has put data = " + data);
+					threadLocal.set(data);
+					new A().get();
+					new B().get();
+				}
+			}).start();
+		}
+		
+	}
+	
+	static class A {
+		public void get() {
+			int data = (int)threadLocal.get();
+			System.out.println("A from " + Thread.currentThread().getName() + " get data : " + data);
+		}
+	}
+	
+	static class B {
+		public void get() {
+			int data = (int)threadLocal.get();
+			System.out.println("A from " + Thread.currentThread().getName() + " get data : " + data);
+		}
+	}
+}
